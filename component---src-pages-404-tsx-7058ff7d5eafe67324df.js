e45b215cc3d02b6678ab6d64429cc293aadd6d01
jordanmceachern <jormceachern@gmail.com>
2023-12-14 11:43:23 -0400

    Updates

---
+++ component---src-pages-404-tsx-7058ff7d5eafe67324df.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[218],{2513:function(e,t,a){a.r(t);var r=a(7294),l=a(5894);t.default=e=>{let{location:t}=e;return r.createElement(l.Z,{currentPathname:t.pathname},r.createElement("h1",{className:"mx-auto my-24"},"404: Page Not Found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-7058ff7d5eafe67324df.js.map
