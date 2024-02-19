cc47ba5721bb6b9f8f22877b7ddfaf3c5c1f1464
Jordan McEachern <jmceachern@l743y6fjkw.home>
2024-02-19 09:45:49 -0400

    Updates

---
+++ component---src-pages-404-tsx-2738289b659e3ef6138b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[453],{60219:function(e,t,a){a.r(t);var r=a(96540),l=a(15476);t.default=e=>{let{location:t}=e;return r.createElement(l.A,{currentPathname:t.pathname},r.createElement("h1",{className:"mx-auto my-24 h-screen"},"404: Page Not Found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-2738289b659e3ef6138b.js.map
---
+++ component---src-pages-404-tsx-7f3d053d2765dafed3af.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[453],{731:function(e,t,a){a.r(t);var r=a(6540),l=a(1206);t.default=e=>{let{location:t}=e;return r.createElement(l.A,{currentPathname:t.pathname},r.createElement("h1",{className:"mx-auto my-24 h-screen"},"404: Page Not Found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-7f3d053d2765dafed3af.js.map
